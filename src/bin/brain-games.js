06b8f5f289864c3ea04cdc4531d1fd0af595e3dc
Sergei Suchkov <rafaelfederer335@gmail.com>
2019-07-25 16:20:48 +0300

    step 4.3

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,3 @@
 #!/usr/bin/env node
-console.log(`Welcome to the Brain Games!\n `);
 import {userName} from '..';
-console.log(`Hello, ${userName}!`);
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import readlineSync from 'readline-sync';
+console.log('Welcome to the Brain Games!\n ');
 const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
 export {userName};
