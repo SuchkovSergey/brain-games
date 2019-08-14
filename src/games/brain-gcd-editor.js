637691ae87db581ff55f60dd3e510568844e2c62
Sergei Suchkov <rafaelfederer335@gmail.com>
2019-08-14 21:43:10 +0300

    step 1.2.2

--- src/games/brain-gcd-editor.js
+++ src/games/brain-gcd-editor.js
@@ -4,6 +4,19 @@ import {
   greeting, greetingName, loop, getRandomInt,
 } from '..';
 
+const correctAnswerFinder = (argument2) => {
+  const numOne = Number(car(argument2));
+  const numTwo = Number(cdr(argument2));
+  let counter = 1;
+  let j = 1;
+  while (j <= Math.min(numOne, numTwo)) {
+    if ((numOne % j === 0) && (numTwo % j === 0)) {
+      counter = j;
+    }
+    j += 1;
+  }
+  return counter;
+};
 
 const gcd = () => {
   greeting();
@@ -12,19 +25,6 @@ const gcd = () => {
   greetingName(userName);
   const argument = () => cons(getRandomInt(1, 100), getRandomInt(1, 100));
   const questionPrint = argument1 => console.log(`Question: ${Number(car(argument1))} and ${Number(cdr(argument1))}`);
-  const correctAnswerFinder = (argument2) => {
-    const numOne = Number(car(argument2));
-    const numTwo = Number(cdr(argument2));
-    let counter = 1;
-    let j = 1;
-    while (j <= Math.min(numOne, numTwo)) {
-      if ((numOne % j === 0) && (numTwo % j === 0)) {
-        counter = j;
-      }
-      j += 1;
-    }
-    return counter;
-  };
   loop(correctAnswerFinder, userName, questionPrint, argument);
 };
 
