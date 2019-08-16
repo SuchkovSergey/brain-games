4c1eb69e659775d5a1d39826dd7a59e65295cc08
Sergei Suchkov <rafaelfederer335@gmail.com>
2019-08-16 21:00:03 +0300

    step 1.2.7

--- src/games/even.js
+++ src/games/even.js
@@ -4,7 +4,7 @@ import {
 
 const argument = () => getRandomInt(1, 100);
 const isEven = num => num % 2 === 0;
-const correctAnswerFinder = num => isEven(num) ? 'yes' : 'no';
+const correctAnswerFinder = num => (isEven(num) ? 'yes' : 'no');
 const questionPrint = argument1 => evenQuestion(`Your number: ${Number(argument1)}`);
 const task = () => evenTask('Answer "yes" if number even otherwise answer "no"');
 const isEvenNum = () => gameMaker(task, correctAnswerFinder, questionPrint, argument);
